c8eee1c1f11ddd8e2a2cf214fa36d452c55b85ea
Andrew Deaver <deaver.andrew@gmail.com>
2018-04-09 19:44:50 -0400

    Finished Exercise 9

--- exercises/ex09/newshound.c
+++ exercises/ex09/newshound.c
@@ -11,7 +11,7 @@ Modified by Allen Downey.
 #include <stdlib.h>
 #include <errno.h>
 #include <sys/types.h>
-#include <wait.h>
+#include <sys/wait.h>
 
 
 void error(char *msg)
@@ -20,13 +20,24 @@ void error(char *msg)
     exit(1);
 }
 
+void rss_feed_print(char* rss_feed, char* search_phrase, const char* PYTHON, const char* SCRIPT) {
+    char var[255];
+	sprintf(var, "RSS_FEED=%s", rss_feed);
+	char *vars[] = {var, NULL};
+
+	int res = execle(PYTHON, PYTHON, SCRIPT, search_phrase, NULL, vars);
+	if (res == -1) {
+		error("Can't run script.");
+	}
+}
+
 int main(int argc, char *argv[])
 {
     if (argc < 2) {
         fprintf(stderr, "Usage: %s <search phrase>\n", argv[0]);
         return 1;
     }
-    const char *PYTHON = "/usr/bin/python2";
+    const char *PYTHON = "/usr/bin/python2.6";
     const char *SCRIPT = "rssgossip.py";
     char *feeds[] = {
         "http://www.nytimes.com/services/xml/rss/nyt/Africa.xml",
@@ -37,16 +48,29 @@ int main(int argc, char *argv[])
     };
     int num_feeds = 5;
     char *search_phrase = argv[1];
-    char var[255];
 
+    pid_t pid;
     for (int i=0; i<num_feeds; i++) {
-        sprintf(var, "RSS_FEED=%s", feeds[i]);
-        char *vars[] = {var, NULL};
+		pid = fork();
 
-        int res = execle(PYTHON, PYTHON, SCRIPT, search_phrase, NULL, vars);
-        if (res == -1) {
-            error("Can't run script.");
-        }
+		if (pid == 0) {
+			rss_feed_print(feeds[i], search_phrase, PYTHON, SCRIPT);
+			exit(0);
+		}
     }
+    
+	int status;
+    for (int i=0; i<num_feeds; i++) {
+        pid = wait(&status);
+
+        if (pid == -1) {
+            fprintf(stderr, "wait failed: %s\n", strerror(errno));
+            perror(argv[0]);
+            exit(1);
+        }
+
+        status = WEXITSTATUS(status);
+        //printf("Child %d exited with error code %d.\n", pid, status);
+	}
     return 0;
 }
